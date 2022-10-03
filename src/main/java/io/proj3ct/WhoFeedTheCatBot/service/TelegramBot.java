62f858e9a389b2fce627411a0902362042309d49
nmnm_220 <mosnikju@gmail.com>
2022-10-03 18:20:59 +0300

    init commit

---
+++ src/main/java/io/proj3ct/WhoFeedTheCatBot/service/TelegramBot.java
@@ -0,0 +1,48 @@
+package io.proj3ct.WhoFeedTheCatBot.service;
+
+import io.proj3ct.WhoFeedTheCatBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig config;
+    public TelegramBot (BotConfig config) {
+        this.config = config;
+    }
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        long chatId = update.getMessage().getChatId();
+
+        if (update.hasMessage() && update.getMessage().hasText())
+        {
+            sendMessage(chatId, update.getMessage().getFrom().getFirstName() + " " + update.getMessage().getText());
+        }
+
+    }
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
