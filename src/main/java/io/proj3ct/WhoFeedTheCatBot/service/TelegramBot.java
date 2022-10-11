73075c9bcd1fc69e3a85e9fd945e9a74541c0364
nmnm_220 <mosnikju@gmail.com>
2022-10-11 18:14:42 +0300

    added message parse

--- src/main/java/io/proj3ct/WhoFeedTheCatBot/service/TelegramBot.java
+++ src/main/java/io/proj3ct/WhoFeedTheCatBot/service/TelegramBot.java
@@ -2,16 +2,26 @@ package io.proj3ct.WhoFeedTheCatBot.service;
 
 import io.proj3ct.WhoFeedTheCatBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
+    @Value("${bot.chat.id}")
+    private long chatId;
+    private boolean catFed = false;
+    @Value("${cat.name}")
+    private String catName;
     public TelegramBot (BotConfig config) {
         this.config = config;
     }
@@ -28,13 +38,33 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
-        long chatId = update.getMessage().getChatId();
-
-        if (update.hasMessage() && update.getMessage().hasText())
+        /*if (update.getMessage().getText().equals("test"))
         {
-            sendMessage(chatId, update.getMessage().getFrom().getFirstName() + " " + update.getMessage().getText());
-        }
+            long chatId = update.getMessage().getChatId();
+            sendMessage(chatId, String.valueOf(chatId));
+        }*/
 
+        if (update.hasMessage() && update.getMessage().hasText() & parseMessage(update.getMessage().getText()))
+        {
+            if (!catFed) {
+                long chatId = update.getMessage().getChatId();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("k:mm");
+                Date currDate = new Date();
+
+                String catNameDec = catName.substring(0, catName.length() - 1) + 'у';
+                sendMessage(chatId, update.getMessage().getFrom().getFirstName() + " покормил(а) " + catNameDec + " в " + simpleDateFormat.format(currDate) + "." +
+                        "\nУра! Теперь " + catName + " сыт.");
+                catFed = true;
+                this.chatId = update.getMessage().getChatId();
+            }
+            else
+            {
+                long chatId = update.getMessage().getChatId();
+
+                String catNameDec = catName.substring(0, catName.length() - 1) + 'у';
+                sendMessage(chatId, catNameDec + " уже покормили раньше.");
+            }
+        }
     }
     private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
@@ -45,6 +75,23 @@ public class TelegramBot extends TelegramLongPollingBot {
             execute(message);
         } catch (TelegramApiException e) {
             log.error("Error occurred: " + e.getMessage());
+        }
+    }
+    private boolean parseMessage(String message)
+    {
+        if (message != null) {
+            message = message.toLowerCase();
+            return message.contains("покорм");
+        }
+        return false;
+    }
+    @Scheduled(cron = "* * 7,14,22 * * *")
+    private void feedReminder()
+    {
+        catFed = false;
+        if (chatId != 0) {
+            String messageToSend = catName + " хочет кушать, кто-нибудь покормите его!";
+            sendMessage(chatId, messageToSend);
         }
     }
 }
